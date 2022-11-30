4fd21b4f2a6259967a2a3750762ffc80f773faea
Ruslan9111 <ruslan_ru911@mail.ru>
2022-11-30 22:30:44 +0500

    30.11.2022 test3

---
+++ tests/func/test_add.py
@@ -0,0 +1,33 @@
+import pytest
+import tasks
+from tasks import Task
+
+
+def test_add_returns_valid_id():
+    """tasks.add(valid task) должен возвращать целое число"""
+    # GIVEN an initialized tasks db
+    # WHEN a new task is added
+    # THEN returned task_id is of type int
+    new_task = Task('sit in chair', owner='me', done=True)
+    task_id = tasks.add(new_task)
+    assert isinstance(task_id, int)
+
+@pytest.mark.smoke
+def test_added_task_has_id_set():
+    """Убедимся, что поле task_id установлено tasks.add()."""
+    # GIVEN an initialized tasks db
+    #   AND a new task is added
+    new_task = Task('sit in chair', owner='me', done=True)
+    task_id = tasks.add(new_task)
+
+    # WHEN task is retrieved
+    task_from_db = tasks.get(task_id)
+
+    # THEN task_id matches id field
+    assert task_from_db.id == task_id
+
+
+
+
+
+
