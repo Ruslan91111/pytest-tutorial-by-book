8974aa263d53ca4b161738b1256f3034f6e35eff
Ruslan9111 <ruslan_ru911@mail.ru>
2023-01-18 15:52:31 +0500

    ch_5_getoption_--nice

--- ch5/c/tasks_proj/tests/conftest.py
+++ ch5/c/tasks_proj/tests/conftest.py
@@ -1,3 +1,10 @@
+"""
+Определить фикстуры для использования в проекте.
+В этом файле также настраиваем отображение в терминале сообщений о падении теста.
+При помощи опции --nice
+"""
+
+
 import pytest
 import tasks
 from tasks import Task
@@ -5,23 +12,26 @@ from tasks import Task
 
 # область сеанса
 # @pytest.fixture(scope='session', params=['tiny',])
-@pytest.fixture(scope='session', params=['tiny', 'mongo'])
+@pytest.fixture(scope='session')
 def tasks_db_session(tmpdir_factory, request):
-    """ Подключение к БД перед тестами, отключение после."""
+    """ Подключение к БД один раз перед тестами, отключение после."""
+    # создать временный каталог
     temp_dir = tmpdir_factory.mktemp('temp')
-    tasks.start_tasks_db(str(temp_dir), request.param)
-    yield
+    # поключиться к временной БД
+    tasks.start_tasks_db(str(temp_dir), 'tiny')
+    yield # выполнение тестовых функций
+
     tasks.stop_tasks_db()
 
 
 @pytest.fixture()
 def tasks_db(tasks_db_session):
-    """Опустошить БД"""
+    """Очистить БД"""
     tasks.delete_all()
 
 
-# фикстура с тремя разными разными данными
-@pytest.fixture(scope='session')
+# Фикстура с данными (тремя разными).
+@pytest.fixture()
 def tasks_just_a_few():
     """Все задачи и исполнители уникальны."""
     return (
@@ -31,7 +41,7 @@ def tasks_just_a_few():
     )
 
 
-# фикстура с данными: задачами, распределенными по несколько на одного исполнителя
+# Фикстура с данными: задачами, распределенными по несколько на одного исполнителя.
 @pytest.fixture(scope='session')
 def tasks_mult_per_owner():
     """Несколько исполнителей с несколькими задачами каждый."""
@@ -64,18 +74,18 @@ def db_with_multi_per_owner(tasks_db, tasks_multi_per_owner):
         tasks.add(t)
 
 
-# # функция - хук для вывода ободряющего сообщения
+# Функция - хук для вывода ободряющего сообщения
 # def pytest_report_header():
 #     return "Thanks for running the test."
-#
-#
+
 # # функция - хук для вывода 'O' вместо 'F'
 # def pytest_report_teststatus(report):
 #     if report.when == 'call' and report.failed:
 #             return (report.outcome, 'O', 'OPPORTUNITY for improvement')
 
 
-# Добавление параметра командной строки, --nice, чтобы изменения статуса
+
+# Добавление параметра командной строки, "--nice", чтобы изменения статуса
 # происходили, только если подставить --nice.
 def pytest_addoption(parser):
     """Включает nice функцию с опцией --nice."""
@@ -83,15 +93,17 @@ def pytest_addoption(parser):
     group.addoption("--nice", action="store_true",
                     help="nice: turn failures into opportunities")
 
+
 def pytest_report_header(config):
     """Благодарность тестеру за выполнение тестов."""
     if config.getoption('nice'):
         return "Thanks for running the tests."
 
-def pytest_report_teststatus(report):
+
+def pytest_report_teststatus(report, config):
     """Превращает неудачи в возможности."""
     if report.when == 'call':
-        if report.failed and pytest.config.getoption('nice'):
+        if report.failed and config.getoption('nice'):
             return (report.outcome, 'O', 'OPPORTUNITY for improvement')
 
 
